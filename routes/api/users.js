b37664f105aa5493f0e60cffbea2dae5259f5b52
alex-s85 <worldmaster5558@hotmail.com>
2021-05-26 12:48:55 +0200

    Passport JWT Authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravartar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const keys = require('../../config/keys');
 
@@ -96,5 +97,19 @@ router.post('/login', (req, res) => {
         });
     });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    });
+  }
+);
 
 module.exports = router;
