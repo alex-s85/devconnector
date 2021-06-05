3374fb1a1b43e50b351219c6524278703102d545
alex-s85 <worldmaster5558@hotmail.com>
2021-06-05 11:22:33 +0200

    Delete Education and Experience Routes. Add the case that the instance doesnot exsit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,8 @@ const validateEducationInput = require('../../validation/education');
 
 // Load Profile Model
 const Profile = require('../../models/Profile');
+// Load User Model
+const User = require('../../models/User');
 
 // @route   GET api/profiles/test
 // @desc    Tests profiles route
@@ -260,6 +262,83 @@ router.post(
 
         profile.save()
           .then(profile => res.json(profile))
+          .catch(err => res.status(400).json(err));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete(
+  '/experience/:exp_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        if (removeIndex < 0) {
+          return res.status(404).json({ noexperience: 'There is no experience item matching the id.'});
+        }
+
+        // Splice out of array
+        profile.experience.splice(removeIndex, 1);
+
+        // save
+        profile.save()
+          .then(profile => res.json(profile))
+          .catch(err => res.status(400).json(err));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile/education/:exp_id
+// @desc    Delete education from profile
+// @access  Private
+router.delete(
+  '/education/:edu_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
+
+        if (removeIndex < 0) {
+          return res.status(404).json({ noeducation: 'There is no education item matching the id.'});
+        }
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // save
+        profile.save()
+          .then(profile => res.json(profile))
+          .catch(err => res.status(400).json(err));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+      .then(() => {
+        User.findOneAndRemove({ _id: req.user.id })
+          .then(() => res.json({ success: true}))
           .catch(err => res.status(400).json(err));
       })
       .catch(err => res.status(404).json(err));
