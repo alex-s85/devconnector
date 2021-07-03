ce039eb527fa809bfe7f55530120dc1742df750f
alex-s85 <worldmaster5558@hotmail.com>
2021-07-03 10:13:32 +0200

    Testing Registration With Our Form - No Redux Yet
    Solve CORS error

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Register extends Component {
   constructor() {
@@ -25,7 +26,11 @@ export default class Register extends Component {
       password: this.state.password,
       password2: this.state.password2,
     }
-    console.log(newUser)
+
+    // axios.defaults.baseURL = 'http://localhost:5000';
+    axios.post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
   }
 
   render() {
