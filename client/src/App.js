2ec3c963a2a2026c076e084f6a1d4518942fbca1
alex-s85 <worldmaster5558@hotmail.com>
2021-08-12 11:38:43 +0200

    043 Login Form Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -10,6 +14,17 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token to header auth
+  setAuthToken(localStorage.jwtToken);
+
+  // Decode tokent and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 function App() {
   return (
