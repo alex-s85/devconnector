3dbf75ad69751f5c0d7fd26024e954cec5d16551
alex-s85 <worldmaster5558@hotmail.com>
2021-09-23 10:40:30 +0200

    047 Spinner Component Dashboard Start

---
+++ client/src/components/common/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import spinner from './spinner.gif';
+
+export default () => {
+  return (
+    <div>
+      {/* <img
+        src={spinner}
+        alt="Loading..."
+        style={{ width: '200px', margin: 'auth', display: 'block' }}
+      /> */}
+      <h4>Loading...</h4>
+    </div>
+  );
+};
