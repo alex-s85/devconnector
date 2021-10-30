36a62b380bc271054b47f5f22aa26a30c21e4c7d
alex-s85 <worldmaster5558@hotmail.com>
2021-10-30 11:27:16 +0200

    073 Comment Display/Delete and Clear errors

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 
 import PostItem from '../posts/PostItem';
 import CommentForm from './CommentForm';
+import CommentFeed from './CommentFeed';
 import Spinner from '../common/Spinner';
 
 import { getPost } from '../../actions/postActions';
@@ -24,6 +25,7 @@ class Post extends Component {
         <div>
           <PostItem post={post} showActions={false} />
           <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
